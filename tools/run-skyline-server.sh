78e015b9664a04515e438b4b46938caea317e9fb
msojocs <jiyecafe@gmail.com>
2026-09-20 19:22:28 +0800

    perf: skyline启动脚本优化

--- tools/run-skyline-server.sh
+++ tools/run-skyline-server.sh
@@ -8,6 +8,7 @@ docker run -d --rm \
     -e HOST_UID=$(id -u) \
     -e HOST_GID=$(id -g) \
     -v "/dev/shm:/dev/shm" \
-    -v "$PWD:$PWD:ro" \
+    -v "$root_dir:$root_dir:ro" \
+    -v "$HOME/.config/wechat-devtools:$HOME/.config/wechat-devtools:ro" \
     --name wechat_devtools_server \
     ghcr.io/msojocs/skyline-client-server:master
